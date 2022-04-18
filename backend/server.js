a585ae6b4086098ce6c660045a420facf93f81a2
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-04-18 12:54:29 -0400

    added some components

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const compression = require('compression');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -10,6 +11,19 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+// check that app is running on production mode
+if (process.env.NODE_ENV === 'production') {
+    //forces https on the website
+    app.use(compression());
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 //api pathway
 app.use('/api/jobrequests', require('./routes/jobrequestsRoutes'));
