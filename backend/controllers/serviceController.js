346b39db926f904dec56c7768b57cc4485212d48
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-04-18 10:55:17 -0400

    add the asynchandler

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,31 +1,36 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get all Service
 // @route GET /api/services
 // @access Private
-const getServices = async (req, res) => {
+const getServices = asyncHandler (async (req, res) => {
     res.status(200).json({message: 'Get Services'});
-}
+})
 
 // @desc Set Service
 // @route POST /api/services
 // @access Private
-const setService = async (req, res) => {
+const setService = asyncHandler (async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field');
+    } 
     res.status(200).json({message: 'Set Services'});
-}
+})
 
 // @desc Update Service
 // @route PUT /api/services/:id
 // @access Private
-const updateService = async (req, res) => {
+const updateService = asyncHandler (async (req, res) => {
     res.status(200).json({message: `update Services ${req.params.id}`});
-}
-
+})
 
 // @desc Delete Service
 // @route DELETE /api/services/:id
 // @access Private
-const deleteService = async (req, res) => {
+const deleteService = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Delete Services ${req.params.id}`});
-}
+})
 
 module.exports = {
     getServices, setService, updateService, deleteService
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//api pathway
 app.use('/api/services', require('./routes/serviceRoutes'));
+
+//pathway to the error
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
