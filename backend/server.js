10078280e53d0bdc4fb391cf0f2937ca203b7c9a
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-04-18 09:56:48 -0400

    added react

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("this is the server")
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
