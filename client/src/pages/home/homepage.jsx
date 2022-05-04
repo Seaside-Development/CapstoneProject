6411195fb6841e201a91c252e4252de934733efc
khareenp <khareenp1@gmail.com>
2022-05-04 11:26:27 -0400

    added create account card

--- client/src/pages/home/homepage.jsx
+++ client/src/pages/home/homepage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-    return (
-        <div>
-             <p className=" bg-red-500"> TEster</p>
-        <h1>Home Page</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
 
 export default HomePage;
