3ecdd6e32f8cacd257f08adb5378c2e758e3d2bf
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-05-11 11:36:02 -0400

    Delete backend directory

--- backend/routes/jobrequestsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getJobrequests, setJobrequest, updateJobrequest, deleteJobrequest} = require('../controllers/jobrequestsController');// Importing the serviceController
-
-router.route('/').get(getJobrequests).post(setJobrequest);
-router.route('/:id').put(updateJobrequest).delete(deleteJobrequest);
-
-module.exports = router;
