c04badd140436d8edf6fbe2bef8a69d6777fcb66
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-05-17 19:46:43 -0400

    add contractor overview pages

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import Header from "./components/header/header.components";
 import Footer from "./components/footer/footer.components";
 
 //page import
-import DashboardPage from './pages/dashboard/dashboardpage';
 import HomePage from "./pages/home/homepage";
+import DashboardPage from './pages/dashboard/dashboardpage';
 import AboutUsPage from "./pages/about-us/aboutus-page";
 import UserAccountPage from './pages/account/user-account-page';
 import ContactUsPage from "./pages/contact-us/contact-us-page";
 import ContractorAccountPage from './pages/contractor-account/contractor-account-page';
 import ContractorCreationPage from './pages/contractor-creation/contractor-creation-page';
-import CustomerReviewPage from './pages/customer-review/customer-review-page';
 import JobDescriptionPage from "./pages/job-description/job-description-page";
 import ReviewsPage from "./pages/reviews/reviewspage";
 import SigninSignupPage from "./pages/singin-and-signup/signin-and-signup-page";
 import ErrorPage from "./pages/error/error-page";
 import JobSearchPage from "./pages/job-description/job-search-page";
+import ContractorOverviewPage from './pages/contractor-overview/contractor-Overview-Page'
+
 
 const App = () => {
   return (
@@ -29,15 +30,15 @@ const App = () => {
             <Route path='/' element={<HomePage/>}/>
             <Route path='/dashboard' element={<DashboardPage/>}/>
             <Route path='/about-us' element={<AboutUsPage/>}/>
-            <Route path='/accounts' element={<UserAccountPage/>}/>
+            <Route path='/myAccount' element={<UserAccountPage/>}/>
             <Route path='/contact-us' element={<ContactUsPage/>}/>
             <Route path='/contractor-profile' element={<ContractorAccountPage/>}/>
-            <Route path='/service-creation' element={<ContractorCreationPage/>}/>
-            <Route path='/customer-reveiws' element={<CustomerReviewPage/>}/>
+            <Route path='/ContractorCreationPage' element={<ContractorCreationPage/>}/>
             <Route path='/job-description' element={<JobDescriptionPage/>}/>
+            <Route path='/contractor' element={<ContractorOverviewPage/>}/>
             <Route path='/reviews' element={<ReviewsPage/>}/>
             <Route path='/signin-signup' element={<SigninSignupPage/>}/>
-            <Route path='/job-search' element={<JobSearchPage/>}/>
+            <Route path='/jobCreation' element={<JobSearchPage/>}/>
             <Route path='*' element={ErrorPage}/>
         </Routes>
       <Footer/>
---
+++ src/pages/contractor-overview/contractor-Overview-Page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContractorOverviewPage = () => {
+    return (
+        <div>
+            <h1>Contractor Overview Page</h1>
+        </div>
+    );
+}   
+
+export default ContractorOverviewPage;
