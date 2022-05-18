07d9ba06077b5a0f5a0f942b938a24af445514db
khareenp <khareenp1@gmail.com>
2022-05-17 20:22:49 -0400

    text changes

--- src/pages/singin-and-signup/signin-and-signup-page.jsx
+++ src/pages/singin-and-signup/signin-and-signup-page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const SigninSignupPage = () => {
-
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -35,7 +34,7 @@ const SigninSignupPage = () => {
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            Login to your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
@@ -50,7 +49,12 @@ const SigninSignupPage = () => {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+            <form
+              className="space-y-6"
+              action="#"
+              method="POST"
+              onSubmit={handleSubmit}
+            >
               <div>
                 <label
                   htmlFor="email"
@@ -120,7 +124,7 @@ const SigninSignupPage = () => {
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Sign in
+                  Login
                 </button>
               </div>
             </form>
