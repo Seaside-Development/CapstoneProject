377599a8af01bfd6808df60600994222ac909175
khareenp <khareenp1@gmail.com>
2022-05-17 21:14:22 -0400

    update

--- src/pages/singin-and-signup/signin-and-signup-page.jsx
+++ src/pages/singin-and-signup/signin-and-signup-page.jsx
@@ -9,7 +9,7 @@ const SigninSignupPage = () => {
   return (
     <div className=" h-screen w-screen">
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md pb-4">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Login to your account
           </h2>
@@ -25,7 +25,7 @@ const SigninSignupPage = () => {
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="bg-white py-8 px-4 shadow lg:rounded-lg sm:px-10">
             <form
               className="space-y-6"
               action="#"
