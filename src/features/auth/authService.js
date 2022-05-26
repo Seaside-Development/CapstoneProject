194b942136af79fc36828b297b1105813da2981e
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-05-26 14:18:42 -0400

    removed dup

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 //const API_URL = '/api/users/'
+const   API_URL = 'https://seaside-bb.herokuapp.com/'
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(`https://seaside-bb.herokuapp.com/api/users/register`, userData)
+  const response = await axios.post(`${API_URL}api/users/register`, userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
